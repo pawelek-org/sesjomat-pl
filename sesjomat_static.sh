42f44046d9fbde88a22233ce9d1c9ccfb64fecf1
pawelek-org <github@pawelek.e4ward.com>
2019-11-09 12:46:27 +0100

    Update

--- sesjomat_static.sh
+++ sesjomat_static.sh
@@ -5,4 +5,11 @@
 ./sesjomat_lunr.sh
 ./vendor/bin/drush cron
 ./vendor/bin/drush tome:static -l https://sesjomat.pl
-cp ./themes/sm/img/sesjomat-logo-og.jpg ./html/themes/custom/sm/img/sesjomat-logo-og.jpg
+cp -f ./themes/sm/img/sesjomat-logo-og.jpg ./html/themes/custom/sm/img/sesjomat-logo-og.jpg
+cp -f ./themes/sm/img/favicon.ico ./html/favicon.ico
+mkdir -p ./html/core/assets/vendor/jquery
+cp -f ./web/core/assets/vendor/jquery/jquery.js ./html/core/assets/vendor/jquery/jquery.js
+mkdir -p ./html/modules/contrib/xmlsitemap/xsl
+cp -f ./web/modules/contrib/xmlsitemap/xsl/jquery.tablesorter.min.js ./html/modules/contrib/xmlsitemap/xsl/jquery.tablesorter.min.js
+cp -f ./web/modules/contrib/xmlsitemap/xsl/xmlsitemap.xsl.js ./html/modules/contrib/xmlsitemap/xsl/xmlsitemap.xsl.js
+cp -f ./web/modules/contrib/xmlsitemap/xsl/xmlsitemap.xsl.css ./html/modules/contrib/xmlsitemap/xsl/xmlsitemap.xsl.css
